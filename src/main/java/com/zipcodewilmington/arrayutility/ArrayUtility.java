3a35e74b874ade1a27a3d7c7b7420adecbb499fd
andyzheng679 <andyzheng679@gmail.com>
2024-04-02 22:24:14 -0400

    dups

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,43 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+    private T[] array;
+
+    public ArrayUtility(T[] array){
+        this.array = array;
+    }
+    public int countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        List<T> merged = new ArrayList<>();
+        merged.addAll(Arrays.asList(this.array));
+        merged.addAll(Arrays.asList(arrayToMerge));
+
+        int counter = 0;
+
+        for(int i = 0; i < merged.size(); i++){
+            if(merged.get(i).equals(valueToEvaluate)){
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        return null;
+    }
+
+    public int getNumberOfOccurrences(T valueToEvaluate) {
+        return  0;
+    }
+
+    public T[] removeValue(T valueToRemove) {
+        return null;
+    }
 }
